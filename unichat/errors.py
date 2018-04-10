f592aaaf5474f41732fcf75d3b6edf1f5e649a1a
Perry Cate <NPCate01@gmail.com>
2018-04-09 23:24:57 -0400

    Add basic conversations endpoint
    Not implemented: paging, last_updated.
    
    last_updated is going to take another query or some other workaround.

--- unichat/errors.py
+++ unichat/errors.py
@@ -1,12 +1,44 @@
 import falcon
 
+
 def set_mappings(app):
     """
     Configure error handlers for exceptions that might be raised. Unhandled
     errors trigger an HTTP 500.
     """
     app.add_error_handler(NotImplementedError, _not_implemented)
+    # Most generic handler must come first
+    app.add_error_handler(ServerError, _server_error)
+    app.add_error_handler(AuthenticationError, _auth_error)
 
+
+#
+# Error Definitions
+#
+
+class ServerError(Exception):
+    """
+    Generic error raised for any error returned from an external service.
+    """
+    pass
+
+class AuthenticationError(ServerError):
+    """
+    Raised when a translator cannot authenticate with its service with the
+    given auth token.
+    """
+    pass
+
+#
+# Error Handling Methods
+#
 
 def _not_implemented(exception, request, response, params):
-    response.status = falcon.HTTP_501
+    response.status = falcon.HTTP_501 # HTTP Not Implemented
+
+
+def _server_error(exception, request, response, params):
+    response.status = falcon.HTTP_502 # HTTP Bad Gateway
+
+def _auth_error(exception, request, response, params):
+    response.status = falcon.HTTP_401
--- unichat/translators/slack.py
+++ unichat/translators/slack.py
@@ -1,6 +1,9 @@
 """
 Translates incoming requests into proper queries against GroupMe's public API.
 """
+import json
+
+from unichat.errors import AuthenticationError, ServerError
 from unichat.models import User, Conversation, ConversationCollection, \
                                 Message, MessageCollection
 from unichat.util import make_request
@@ -9,16 +12,41 @@ from unichat.translators import Translator
 
 class Slack(Translator):
 
-    url_base = ''
+    SERVICE = "Slack" # For identification in errors, etc.
+    URL_BASE = 'https://slack.com/api'
     def get_users(self, conversation_id, auth='', page=''):
         raise NotImplementedError
 
     def get_conversations_list(self, auth='', page=''):
-        raise NotImplementedError
+        data = self._make_request('/conversations.list', auth)
+
+        channels = []
+        if 'channels' not in data:
+            raise ServerError
+        for channel in data['channels']:
+            channels.append(Conversation(cid=channel['id'],
+                                         name=channel['name'],
+                                         last_updated='')) # TODO workaround
+        return ConversationCollection(channels, '')
+
 
     def get_conversation(self, conversation_id, auth='', page=''):
         raise NotImplementedError
 
     def get_messages(self, conversation_id, auth='', page=''):
         raise NotImplementedError
+
+    def _make_request(self, endpoint, auth, params={}):
+        # Convenience method to reduce the number of arguments passed around
+
+        data = make_request(Slack.URL_BASE, endpoint, auth, params, False)
+
+        # Check for errors
+        if data['ok'] != True:
+            if 'error' in data and data['error'] == 'not_authed':
+                raise AuthenticationError
+            raise ServerError
+
+        return data
+
 
