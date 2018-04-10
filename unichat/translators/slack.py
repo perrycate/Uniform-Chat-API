cdacfb999b67e4f26da585e2f72823add55b8d60
Perry Cate <NPCate01@gmail.com>
2018-04-10 00:13:23 -0400

    Implement conversations/{id}/messages endpoint
    Included paging the first time around this time.

--- unichat/translators/slack.py
+++ unichat/translators/slack.py
@@ -14,6 +14,7 @@ class Slack(Translator):
 
     SERVICE = "Slack" # For identification in errors, etc.
     URL_BASE = 'https://slack.com/api'
+    PAGING_DONE_TOKEN = 'done'
 
     def get_users(self, conversation_id, auth='', page=''):
         raise NotImplementedError
@@ -34,13 +35,37 @@ class Slack(Translator):
     def get_conversation(self, conversation_id, auth='', page=''):
         data = self._make_request('/conversations.info', auth,
                                   {'channel': conversation_id})
+        # TODO paging
         return Conversation(cid=data['channel']['id'],
                             name=data['channel']['name'],
                             # is last_read good enough for last_updated?
                             last_updated=data['channel']['last_read'])
 
     def get_messages(self, conversation_id, auth='', page=''):
-        raise NotImplementedError
+        if page == Slack.PAGING_DONE_TOKEN:
+            return MessageCollection([], next_page='')
+
+        data = self._make_request('/conversations.history', auth,
+                                  {'channel': conversation_id, 'cursor': page})
+
+        messages = []
+        if ('messages' not in data):
+            raise ServerError
+        for message in data['messages']:
+            messages.append(Message(mid='', # TODO standin: hash of data?
+                                    uid=message['user'],
+                                    user_name='', # TODO
+                                    text=message['text'],
+                                    time=message['ts'])) # TODO uniform format
+
+        if 'has_more' in data and data['has_more']:
+            if 'next_cursor' not in data['response_metadata']:
+                raise ServerError
+            cursor = data['response_metadata']['next_cursor']
+        else:
+            cursor = Slack.PAGING_DONE_TOKEN
+
+        return MessageCollection(messages, next_page=cursor)
 
     def _make_request(self, endpoint, auth, params={}):
         # Convenience method to reduce the number of arguments passed around
@@ -49,8 +74,11 @@ class Slack(Translator):
 
         # Check for errors
         if data['ok'] != True:
-            if 'error' in data and data['error'] == 'not_authed':
-                raise AuthenticationError
+            if 'error' in data:
+                if data['error'] == 'not_authed':
+                    raise AuthenticationError
+                if data['error'] == 'missing_scope':
+                    raise AuthenticationError
             raise ServerError
 
         return data
